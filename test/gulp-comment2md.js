658a8fd9d05e3944e93558b65c8ce9c19013d8e1
pirosikick <pirosikick@gmail.com>
2015-04-07 09:54:39 +0900

    Support renaming output file

--- test/gulp-comment2md.js
+++ test/gulp-comment2md.js
@@ -15,23 +15,70 @@ describe("gulp-comment2md", function () {
     before(function () {
     });
 
-    it("should generate markdown from comments", function (done) {
-      let expected = [
-          "Title"
-        , "====="
-        , ""
-        , "Write description about this markdown"
-        , ""
-      ].join("\n");
+    let expected = {
+      "first.js": [
+                      "Title"
+                    , "====="
+                    , ""
+                    , "Write description about this markdown"
+                    , ""
+                  ].join("\n")
+    };
 
-      gulp.src(fixture("first.js"))
+    it("should generate markdown from comments", function (done) {
+      let filename = "first.js";
+
+      gulp.src(fixture(filename))
         .pipe(comment2md())
         .pipe(streamAssert.length(1))
         .pipe(streamAssert.first((file) => {
-          assert(file.contents.toString() === expected);
+          assert(file.contents.toString() === expected[filename]);
           assert(path.basename(file.path) === "first.md");
         }))
         .pipe(streamAssert.end(done));
+    });
+
+    describe("option", function () {
+
+      describe("when String", function () {
+
+        it("should rename output file to the string", function (done) {
+          let filename = "first.js";
+          let newName = "new-name.md";
+
+          gulp.src(fixture(filename))
+            .pipe(comment2md(newName))
+            .pipe(streamAssert.length(1))
+            .pipe(streamAssert.first((file) => {
+              assert(file.contents.toString() === expected[filename]);
+              assert(path.basename(file.path) === newName);
+            }))
+            .pipe(streamAssert.end(done));
+        });
+
+      });
+
+      describe("when Function", function () {
+
+        it("should rename output file to return value of the function", function (done) {
+          let filename = "first.js";
+          let newName = "return-value-of-function.md";
+          let renameFn = function () {
+            return newName;
+          };
+
+          gulp.src(fixture(filename))
+            .pipe(comment2md(renameFn))
+            .pipe(streamAssert.length(1))
+            .pipe(streamAssert.first((file) => {
+              assert(file.contents.toString() === expected[filename]);
+              assert(path.basename(file.path) === newName);
+            }))
+            .pipe(streamAssert.end(done));
+        });
+
+      });
+
     });
   });
 });
